194d2d8f47d4636027d8a76b99514c295cb0dbc8
shashankkannan <135678936+shashankkannan@users.noreply.github.com>
2025-02-04 22:16:36 -0500

    changes from example branch

--- src/Components/Xx.js
+++ src/Components/Xx.js
@@ -4,7 +4,8 @@ function Xx() {
   return (
     <div>
       <h1>X Page</h1>
-      <p>Content for the X page. After change! Changes from Web and local merged. Change I make before checking out another commit.</p>
+      <p>Content for the X page. After change! Changes from Web and local merged. 
+        Change I make before checking out another commit.</p>
     </div>
   );
 }
